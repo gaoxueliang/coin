b81bc7a40d0863a54f24265da27b0e88d9b18f9e
git <git@git-PC>
2017-11-23 17:33:06 +0800

    V1.0

---
+++ Tomcat-dbcp2/build.cmd
@@ -0,0 +1,4 @@
+"C:\Program Files\Java\jdk1.7.0_80\bin\jar.exe" uvf tomcat-dbcp.jar org\apache\tomcat\dbcp\dbcp2\BasicDataSourceFactory.class
+"C:\Program Files\Java\jdk1.7.0_80\bin\jar.exe" uvf tomcat-dbcp.jar org\apache\tomcat\dbcp\dbcp2\BasicDataSourceFactoryEx.class
+
+pause
